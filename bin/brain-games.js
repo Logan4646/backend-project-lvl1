eadfa4da42bee504dd25da67c76c67b08215492d
Anatolii_Gorstka <Logan46@yandex.ru>
2022-05-27 16:16:56 +0300

    add part od 4th spet in project brain-games - done(ellint task3)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Username } from './src/cli.js';
+
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
 Username();
--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
+
 export const Username = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
