fdb3845c372bece62d2405505ec9de385cdc340e
Anatolii_Gorstka <Logan46@yandex.ru>
2022-05-30 16:47:31 +0300

    finish step 4 in 1st project brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { Username } from './src/cli.js';
-
+import Username from './src/cli.js';
+// Greeting of User
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
 Username();
--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const Username = () => {
+const Username = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+export default Username;
