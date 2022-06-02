e83c447e15e6ade59c8251556970487680fd7d56
Anatolii_Gorstka <Logan46@yandex.ru>
2022-06-02 11:17:17 +0300

    correct 5th step in project brain-games with eslint

--- bin/src/even.js
+++ bin/src/even.js
@@ -1,28 +1,28 @@
 import readlineSync from 'readline-sync';
 
-const randomfunc = (maximum) => {
-    return Math.floor(Math.random() * maximum);
-};
-export const even = () => {
-    console.log('Welcome to the Brain Games!');
-    const UsernameFunction = () => readlineSync.question('May I have your name? ');
-    const User = UsernameFunction();
-    console.log(`Hello, ${User}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    for (let counter = 0; counter < 3; counter += 1) {
-        const Random = randomfunc(100);
-        console.log('Question:', Random);
-        const UserAnswer = readlineSync.question('Your answer: ');
-        if (Random % 2 === 0) {
-            if (UserAnswer === 'yes') {
-                console.log('Correct!');
-            } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was yes. \n Let's try again, ${User}!`);
-        }
-        if (Random % 2 === 1) {
-            if (UserAnswer === 'no') {
-                console.log('Correct!');
-            } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was no. \n Let's try again, ${User}!`);
-        } 
+const randomfunc = (maximum) => Math.floor(Math.random() * maximum);
+
+const even = () => {
+  console.log('Welcome to the Brain Games!');
+  const UsernameFunction = () => readlineSync.question('May I have your name? ');
+  const User = UsernameFunction();
+  console.log(`Hello, ${User}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let counter = 0; counter < 3; counter += 1) {
+    const Random = randomfunc(100);
+    console.log('Question:', Random);
+    const UserAnswer = readlineSync.question('Your answer: ');
+    if (Random % 2 === 0) {
+      if (UserAnswer === 'yes') {
+        console.log('Correct!');
+      } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was yes. \n Let's try again, ${User}!`);
     }
-    console.log(`Congratulations, ${User}!`);
+    if (Random % 2 === 1) {
+      if (UserAnswer === 'no') {
+        console.log('Correct!');
+      } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was no. \n Let's try again, ${User}!`);
+    }
+  }
+  return console.log(`Congratulations, ${User}!`);
 };
+export default even;
