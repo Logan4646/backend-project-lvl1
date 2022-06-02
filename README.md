01e2d4f2cec0cf73a3a6556b12cd58bd013deb92
Anatolii_Gorstka <Logan46@yandex.ru>
2022-06-02 10:35:21 +0300

    finish 5th step in project brain-games

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/Logan4646/backend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Logan4646/backend-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 ![Make lint test](https://github.com/Logan4646/backend-project-lvl1/actions/workflows/make-lint-check.yml/badge.svg)
+https://asciinema.org/a/Ya7rfsuQmjHFqi2B59OrhocUK
--- bin/src/even.js
+++ bin/src/even.js
@@ -1,30 +1,28 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const Username = () => readlineSync.question('May I have your name? ');
-const User = Username();
-console.log(`Hello, ${User}!`);
-
 const randomfunc = (maximum) => {
     return Math.floor(Math.random() * maximum);
 };
 export const even = () => {
+    console.log('Welcome to the Brain Games!');
+    const UsernameFunction = () => readlineSync.question('May I have your name? ');
+    const User = UsernameFunction();
+    console.log(`Hello, ${User}!`);
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    for (let i = 0; i < 3; i += 1) {
+    for (let counter = 0; counter < 3; counter += 1) {
         const Random = randomfunc(100);
         console.log('Question:', Random);
-        const Useranswer = readlineSync.question('Your answer: ');
+        const UserAnswer = readlineSync.question('Your answer: ');
         if (Random % 2 === 0) {
-            if (Useranswer === 'yes') {
+            if (UserAnswer === 'yes') {
                 console.log('Correct!');
-            } else return console.log(`"${Useranswer}" is wrong answer ;(. Correct answer was yes. \n Let's try again, ${User}!`);
+            } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was yes. \n Let's try again, ${User}!`);
         }
         if (Random % 2 === 1) {
-            if (Useranswer === 'no') {
+            if (UserAnswer === 'no') {
                 console.log('Correct!');
-            } else return console.log(`"${Useranswer}" is wrong answer ;(. Correct answer was no. \n Let's try again, ${User}!`);
+            } else return console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was no. \n Let's try again, ${User}!`);
         } 
     }
     console.log(`Congratulations, ${User}!`);
 };
-even();
